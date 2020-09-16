ed221740c5ef18ce04d0b08de2bf0da5bb8be947
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-09-17 01:50:11 +1200

    added api details

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -6,6 +6,16 @@ channels.keys().forEach(channels)
 
 const button = document.querySelector('.button')
 const inputValue = document.querySelector('.inputValue')
-const name = document.querySelector('.name')
-const desc = document.querySelector('.desc')
-const temp = document.querySelector('.temp')
+const name = document.querySelector('.name');
+const desc = document.querySelector('.desc');
+const temp = document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=4e88eaf5d49f3485b223efdf825fbfa1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+.catch(err => alert("Wrong city name!"))
+})
+
+
