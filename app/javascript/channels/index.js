2073a8572843141f612bc8ee4fc10eecdbdb3acb
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-09-17 00:59:29 +1200

    added form

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,9 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const button = document.querySelector('.button')
+const inputValue = document.querySelector('.inputValue')
+const name = document.querySelector('.name')
+const desc = document.querySelector('.desc')
+const temp = document.querySelector('.temp')
