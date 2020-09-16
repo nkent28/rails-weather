ee4d53a1b7fbdbe34e80bb460981f411dc13b82e
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-09-17 03:43:45 +1200

    added weather details

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -9,13 +9,27 @@ const inputValue = document.querySelector('.inputValue')
 const name = document.querySelector('.name');
 const desc = document.querySelector('.desc');
 const temp = document.querySelector('.temp');
+const icon = document.querySelector('.icon');
 
 button.addEventListener('click', function(){
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=4e88eaf5d49f3485b223efdf825fbfa1')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    const nameValue = data['name'];
+    const iconValue = 'http://openweathermap.org/img/w/' + data.weather[0].icon + ".png";
+    const tempValue = Math.round(data.main.temp);
+    const descVaue = data.weather[0].description;
 
-.catch(err => alert("Wrong city name!"))
+
+    name.innerHTML = nameValue;
+    temp.innerHTML = tempValue;
+    icon.innerHTML = iconValue;
+    desc.innerHTML = descVaue;
+
+  })
 })
+
+
+
 
 
